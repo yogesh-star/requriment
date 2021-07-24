d863b971a21e79191aa35594096d4b9925288a0c
yogesh-star <82324973+yogesh-star@users.noreply.github.com>
2021-07-24 19:08:53 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -25,5 +25,5 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-apt-cache madison docker-ce
+apt-cache madison docker-ce -y
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
